ac0bec21e5cafb7896a7f4f0352a2aa6b07f349f
Matej Pavic <matej@Matejs-MacBook-Pro.local>
2023-09-14 20:37:13 +0200

    Change App component to a function

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,40 @@
 import './App.css'
-import { Component } from 'react'
+import { useState } from 'react'
 import { UserClass, UserFunction, UserChildren } from './user'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       users: [
-        { name: 'Ivan', years: 30 },
-        { name: 'Marko', years: 35 },
-        { name: 'Ana', years: 25 },
-      ],
-      tekst: "Proizvoljan tekst"
-    }
-  }
+function App() {
 
-  uvecajGodine = () => {
-    this.setState(prevState => {
-      const updatedUsers = prevState.users.map(user => {
+  const initialUsers = [
+    { name: 'Ivan', years: 30 },
+    { name: 'Marko', years: 35 },
+    { name: 'Ana', years: 25 },
+  ]
+
+  const [users, setUsers] = useState(initialUsers)
+
+  const tekst = "Proizvoljan tekst"
+
+
+  const uvecajGodine = () => {
+    setUsers(prevState => {
+      const updatedUsers = prevState.map(user => {
         return { ...user, years: user.years + 1 }
       })
-      return { users: updatedUsers }
+      return updatedUsers
     })
   }
 
-  render() {
-    const {users, tekst} = this.state
-    return (
-      <>
-        <h1>State</h1>
-        <UserClass name={users[0].name} years={users[0].years} />
-        <UserFunction name={users[1].name} years={users[1].years} />
-        <UserChildren name={users[2].name} years={users[2].years}>
-          {tekst}
-        </UserChildren>
-        <button onClick={this.uvecajGodine}>Uvecaj godine</button>
-      </>
-    )
-  }
+  return (
+    <>
+      <h1>State</h1>
+      <UserClass name={users[0].name} years={users[0].years} />
+      <UserFunction name={users[1].name} years={users[1].years} />
+      <UserChildren name={users[2].name} years={users[2].years}>
+        {tekst}
+      </UserChildren>
+      <button onClick={uvecajGodine}>Uvecaj godine</button>
+    </>
+  )
 }
 
 export default App
